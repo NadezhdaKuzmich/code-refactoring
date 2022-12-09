49f962fe949189edda90a3bea15c38815e441f11
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2022-12-09 15:43:42 +0100

    edit work

--- js/main.js
+++ js/main.js
@@ -1,33 +1,11 @@
 const numOrStr = prompt('input number or string');
 
-switch (isNaN(numOrStr) || numOrStr) {
-    case null: {
-        console.log('ви скасували');
-        break;
-    }
-    case '': {
-        console.log('Empty String');
-        break;
-    }
-    case true: {
-        console.log('number is Ba_NaN');
-        break;
-    }
-    default: {
-        console.log('OK!');
-        break;
-    }
-}
-
-/*
-another way:
-
 switch(true) {
     case numOrStr ==  null: {
         console.log('ви скасували');
         break;
     }
-    case numOrStr == '': {
+    case numOrStr.trim() == '': {
         console.log('Empty String');
         break;
     }
@@ -40,4 +18,3 @@ switch(true) {
         break;
     }
 }
-*/
